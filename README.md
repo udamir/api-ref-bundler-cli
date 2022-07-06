55948232cc43f42a9d3a2011a5973db1ab46e288
Damir <damir@yusipov.ru>
2022-07-07 02:31:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# api-ref-bundler-cli
+<img alt="npm" src="https://img.shields.io/npm/v/api-ref-bundler-cli"> <img alt="npm" src="https://img.shields.io/npm/dm/api-ref-bundler-cli?label=npm"> <img alt="npm type definitions" src="https://img.shields.io/npm/types/api-ref-bundler-cli"> <img alt="GitHub" src="https://img.shields.io/github/license/udamir/api-ref-bundler-cli">
+
+This package provides cli tool to resolve all external references in Json based API document and bundle into single document
+
+## Works perfectly with API specifications
+
+- [JsonSchema](https://json-schema.org/draft/2020-12/json-schema-core.html)
+- [Swagger 2.x](https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md)
+- [OpenApi 3.x](https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.3.md)
+- [AsyncApi 2.x](https://www.asyncapi.com/docs/specifications/v2.4.0)
+
+## Usage
+
+TBD
+
+## Contributing
+When contributing, keep in mind that it is an objective of `api-ref-bundler-cli` to have no package dependencies. This may change in the future, but for now, no-dependencies.
+
+Please run the unit tests before submitting your PR: `npm test`. Hopefully your PR includes additional unit tests to illustrate your change/modification!
+
+## License
+
+MIT
---
+++ index.ts
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import { ApiRefBundler } from "api-ref-bundler"
+import { program } from "commander"
+import { promises as fs } from "fs"
+import path from "path"
+import yaml from "js-yaml"
+
+const resolver = async (sourcePath: string): Promise<object> => {
+  const file = await fs.readFile(path.join(process.cwd(), sourcePath), "utf8")
+  return yaml.load(file) as object
+}
+
+const bundler = async (filename: string, type = "oas3", dest: string = "", format: "yaml" | "json" = "json") => {
+  const definitionsBasePath = type === "oas3" || type === "aas2" ? "/components/schemas" : "/$defs"
+
+  const refparser = new ApiRefBundler(filename, resolver, { definitionsBasePath })
+  const result = await refparser.run()
+  if (refparser.errors.length) {
+    throw new Error(refparser.errors[0])
+  }
+  const name = filename.split("/").slice(-1).join("").replace(/.(json|yaml|yml)$/gi, "") + "-bundle." + format
+  if (format === "yaml") {
+    await fs.writeFile(path.join(process.cwd(), dest, name), yaml.dump(result, { noRefs: true, skipInvalid: true }), { encoding: "utf-8" })
+  } else {
+    await fs.writeFile(path.join(process.cwd(), dest, name), JSON.stringify(result, null, 2), { encoding: "utf-8" })
+  }
+  return path.join(dest, name)
+}
+
+export function cli(args: any) {
+  program 
+    .version('1.0.1')
+    .argument('<source>', 'OpenApi document')
+    .alias('v')
+    .description('Open')
+
+  program
+    .option('-d, --dest [dest]', 'Output destination');
+  program
+    .option('-t, --type [type]', 'Source type');
+  program
+    .option('-f, --format [format]', 'Output format: "json" or "yaml"');
+
+  program.parse(args)
+
+  const options = program.opts()
+  const { type = "oas3", dest, format } = options
+
+  const source = program.args[0]
+
+  if (source) {
+    bundler(source, type, dest, format).then((path) => {
+      console.log("Bundle build:", path)
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
+}
