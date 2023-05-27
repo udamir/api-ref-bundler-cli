a70100add2097c29af2deedbbfa044845a508573
Damir <damir@yusipov.ru>
2023-05-27 15:58:21 +0600

    bump api-ref-bundler version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { ApiRefBundler } from "api-ref-bundler"
+import { bundle } from "api-ref-bundler"
 import { program } from "commander"
 import { promises as fs } from "fs"
 import path from "path"
@@ -12,27 +12,37 @@ const resolver = async (sourcePath: string): Promise<object> => {
   return yaml.load(file) as object
 }
 
-const bundler = async (filename: string, dest: string = "", format: "yaml" | "json" = "json") => {
-
-  const refparser = new ApiRefBundler(filename, resolver)
-  const result = await refparser.run()
-  if (refparser.errors.length) {
-    throw new Error(refparser.errors[0])
-  }
-  const name = filename.split("/").slice(-1).join("").replace(/.(json|yaml|yml)$/gi, "") + "-bundle." + format
+const dumpApi = async (data: any, format: "yaml" | "json"): Promise<string> => {
   if (format === "yaml") {
-    await fs.writeFile(path.join(process.cwd(), dest, name), yaml.dump(result, { noRefs: true, skipInvalid: true }), { encoding: "utf-8" })
-  } else {
-    await fs.writeFile(path.join(process.cwd(), dest, name), JSON.stringify(result, null, 2), { encoding: "utf-8" })
+    return yaml.dump(data, { 
+      noRefs: true,
+      skipInvalid: true
+    })
+  } else if (format === "json") {
+    return JSON.stringify(data, null, 2)
   }
+  return ""
+}
+
+const bundler = async (filename: string, dest: string = "", format: "yaml" | "json" = "json") => {
+  const result = await bundle(filename, resolver, { 
+    hooks: { 
+      onError: (msg) => {
+        throw new Error(msg)
+      } 
+    }
+  })
+  const name = filename.split("/").slice(-1).join("").replace(/.(json|yaml|yml)$/gi, "") + "-bundle." + format
+  const dump = await dumpApi(result, format)
+  await fs.writeFile(path.join(process.cwd(), dest, name), dump, { encoding: "utf-8" })
+
   return path.join(dest, name)
 }
 
 export function cli(args: any) {
   program 
-    .version(version)
+    .version(version, '-v, --version', 'Output the current version')
     .argument('<source>', 'OpenApi document')
-    .alias('v')
     .description('Open')
 
   program
